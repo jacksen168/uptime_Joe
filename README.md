502af847360cf641836459cdfea9dc20a2cbb707
jacksen168 <liu163yong163yu@163.com>
2023-08-26 12:04:14 +0800

    add v1.0 files
    add v1.0 files

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # uptime_Joe
+
+这是一个根据[uptime-status](https://github.com/yb/uptime-status) 修改而来,适用于typecho-Joe主题统计页面的拓展,没什么技术门槛。
+
+## 安装步骤
+
+1. 将该项目文件 **census.php** 替换 Joe 原有 **census.php**文件,并填写好相对应你的js/css文件路径
+2. 修改配置文件 **config.js** 即可
---
+++ config.js
@@ -0,0 +1,21 @@
+window.Config = {
+
+  // 显示标题
+  footerText: '© 2023 ',
+  // 链接名称
+  footerLinkName: 'jacksen168.top',
+  // 链接地址
+  footerLinkUrl: 'https://jacksen168.top',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'm792457160-e66818a9b41a1068298d62b6',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+};
